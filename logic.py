2e2ebaced3d4440679712b11a74854a5e1b6abdd
jarsnow <raderjohna@gmail.com>
2024-11-21 21:06:39 -0600

    this works now for sure

--- logic.py
+++ logic.py
@@ -280,7 +280,7 @@ class MyClient(discord.Client):
         # bottom 3
         for i, result in enumerate(results[-highest_least_shown:]):
             UID, CoinCount = result[0], result[1]
-            target_name = self.get_user_name(UID, user_message)
+            target_name = await self.get_user_name(UID, user_message)
             output += (f"{len(results) - i}. **{target_name}** has {CoinCount} coins. \n")
 
         return output
